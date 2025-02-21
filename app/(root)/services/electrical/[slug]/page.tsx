4f53dc5640b9fa1fd74c0cf8242214f58962d88c
Ayberk Yavas <ayberkyavas9@gmail.com>
2025-02-21 18:50:19 +0300

    feat: :sparkles: Update page layouts, add Loader & NoFoundAlert components, and UI alert

--- app/(root)/services/electrical/[slug]/page.tsx
+++ app/(root)/services/electrical/[slug]/page.tsx
@@ -1,20 +1,92 @@
+import { ImageCard } from "@/components/ImageCard";
 import ServiceDetail from "@/components/ServiceDetail";
 import { client } from "@/sanity/lib/client";
+import { urlFor } from "@/sanity/lib/image";
 import {
-	getRelatedServices,
-	getServiceDetailsBySlug,
+  getRelatedServices,
+  getServiceDetailsBySlug,
 } from "@/sanity/lib/queries";
-const Page = async ({ params }: { params: Promise<{ slug: string }> }) => {
-	const slug = (await params)?.slug || "";
+import type { Service } from "@/sanity/types";
+import type { Metadata } from "next";
+import Link from "next/link";
+import React, { Suspense } from "react";
+import RelatedServices from "../../../../../components/RelatedServices";
+import Loader from "@/components/Loader";
 
-	const serviceData = await client.fetch(getServiceDetailsBySlug, { slug });
-	// const { category } = serviceData;
-	// const relatedServices = await client.fetch(getRelatedServices, { category });
-	return (
-		<>
-			<ServiceDetail data={serviceData} />
-		</>
-	);
+export async function generateMetadata({
+  params,
+}: {
+  params: Promise<{ slug: string }>;
+}): Promise<Metadata> {
+  const slug = (await params)?.slug || "";
+  const service: Service = await client.fetch(getServiceDetailsBySlug, {
+    slug,
+  });
+  return {
+    title: service.title,
+    description: service.description,
+    openGraph: {
+      title: service.title,
+      description: service.description,
+      images: [
+        {
+          url:
+            urlFor(service.image).width(300).height(300).url() ||
+            "/images/placeholder.webp",
+          alt: service.title,
+        },
+      ],
+      url: `https://example.com/services/${service.category.toLowerCase()}/${slug}`,
+    },
+  };
+}
+
+const Page = async ({ params }: { params: Promise<{ slug: string }> }) => {
+  const slug = (await params)?.slug || "";
+
+  const serviceData: Service = await client.fetch(getServiceDetailsBySlug, {
+    slug,
+  });
+  const { category } = serviceData;
+  const relatedServices: Service[] = await client.fetch(getRelatedServices, {
+    category,
+    slug,
+    number: 4,
+  });
+  return (
+    <>
+      <div className="flex w-full flex-col gap-5">
+        <Suspense fallback={<Loader className="h-[500px]" />}>
+          <ServiceDetail data={serviceData} />
+        </Suspense>
+        <Suspense fallback={<Loader className="h-[300px]" />}>
+          {RelatedServices.length > 0 && (
+            <>
+              <hr className="w-full" />
+              <h5 className="h5 font-semibold">Users also viewed</h5>
+              <div className="grid w-full grid-cols-1 gap-5 xs:grid-cols-[1fr_1fr] md:grid-cols-[1fr_1fr_1fr_1fr]">
+                {relatedServices?.map((service: Service) => (
+                  <React.Fragment key={service._id}>
+                    <Link href={service.slug.current}>
+                      <ImageCard
+                        src={
+                          urlFor(service.image).width(500).height(500).url() ||
+                          "/images/placeholder.webp"
+                        }
+                        alt={service.title}
+                        caption={service.title}
+                        className="size-full"
+                      />
+                    </Link>
+                  </React.Fragment>
+                ))}
+              </div>
+            </>
+          )}
+        </Suspense>
+      </div>
+    </>
+  );
 };
 
 export default Page;
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,62 +1,63 @@
 import type { Metadata } from "next";
 import { Poppins, Montserrat } from "next/font/google";
+import "./globals.css";
 import "easymde/dist/easymde.min.css";
 
 const poppins = Poppins({
-	variable: "--font-poppins",
-	weight: ["500", "600", "700", "800"],
-	subsets: ["latin"],
+  variable: "--font-poppins",
+  weight: ["500", "600", "700", "800"],
+  subsets: ["latin"],
 });
 
 const montserrat = Montserrat({
-	subsets: ["latin"],
+  subsets: ["latin"],
 });
 
 export const metadata: Metadata = {
-	title: {
-		template: "%s | Reform Marine",
-		default: "Reform Marine",
-	},
-	description:
-		"Reform Marine activity is providing mechanical and electrical service for ship and supplying spare parts. Our goal is to ensure safety and satisfaction in maritime operations by delivering high-quality service and a wide range of products to our customers in a timely and reliable manner.With our expert team and wide range of services, we offer fast and reliable solutions to all kinds of maritime needs of our customers. Reform Marine is proud to be a reliable service in the maritime world. We constantly improve ourselves and follow innovative technologies to understand the needs of our customers and offer them the most suitable solutions. Reform Marine is a company that set out with the mission of providing innovative and high-quality solutions to the maritime sector.",
-	openGraph: {
-		title: "Marine Services, Marine Spare Parts | Reform Marine",
-		description:
-			"Reform Marine activity is providing mechanical and electrical service for ship and supplying spare parts. Our goal is to ensure safety and satisfaction in maritime operations by delivering high-quality service and a wide range of products to our customers in a timely and reliable manner.With our expert team and wide range of services, we offer fast and reliable solutions to all kinds of maritime needs of our customers. Reform Marine is proud to be a reliable service in the maritime world. We constantly improve ourselves and follow innovative technologies to understand the needs of our customers and offer them the most suitable solutions. Reform Marine is a company that set out with the mission of providing innovative and high-quality solutions to the maritime sector.",
-		siteName: "Reform Marine",
-		url: "https://reformmarine.com",
-		type: "website",
-		locale: "en_US",
-	},
-	applicationName: "Reform Marine",
-	appleWebApp: {
-		title: "Reform Marine",
-		statusBarStyle: "black-translucent",
-	},
-	keywords: [
-		"Reform Marine",
-		"Spare Parts",
-		"Marine Services",
-		"Tuzla Shipyard",
-		"Maritime Services",
-		"Mechanical Service",
-		"Electrical Service",
-	],
-	robots: "index, follow",
+  title: {
+    template: "%s | Reform Marine",
+    default: "Reform Marine",
+  },
+  description:
+    "Reform Marine activity is providing mechanical and electrical service for ship and supplying spare parts. Our goal is to ensure safety and satisfaction in maritime operations by delivering high-quality service and a wide range of products to our customers in a timely and reliable manner.With our expert team and wide range of services, we offer fast and reliable solutions to all kinds of maritime needs of our customers. Reform Marine is proud to be a reliable service in the maritime world. We constantly improve ourselves and follow innovative technologies to understand the needs of our customers and offer them the most suitable solutions. Reform Marine is a company that set out with the mission of providing innovative and high-quality solutions to the maritime sector.",
+  openGraph: {
+    title: "Marine Services, Marine Spare Parts | Reform Marine",
+    description:
+      "Reform Marine activity is providing mechanical and electrical service for ship and supplying spare parts. Our goal is to ensure safety and satisfaction in maritime operations by delivering high-quality service and a wide range of products to our customers in a timely and reliable manner.With our expert team and wide range of services, we offer fast and reliable solutions to all kinds of maritime needs of our customers. Reform Marine is proud to be a reliable service in the maritime world. We constantly improve ourselves and follow innovative technologies to understand the needs of our customers and offer them the most suitable solutions. Reform Marine is a company that set out with the mission of providing innovative and high-quality solutions to the maritime sector.",
+    siteName: "Reform Marine",
+    url: "https://reformmarine.com",
+    type: "website",
+    locale: "en_US",
+  },
+  applicationName: "Reform Marine",
+  appleWebApp: {
+    title: "Reform Marine",
+    statusBarStyle: "black-translucent",
+  },
+  keywords: [
+    "Reform Marine",
+    "Spare Parts",
+    "Marine Services",
+    "Tuzla Shipyard",
+    "Maritime Services",
+    "Mechanical Service",
+    "Electrical Service",
+  ],
+  robots: "index, follow",
 };
 
 export default function RootLayout({
-	children,
+  children,
 }: Readonly<{
-	children: React.ReactNode;
+  children: React.ReactNode;
 }>) {
-	return (
-		<html lang="en">
-			<body
-				className={`${poppins.variable} ${montserrat.className} antialiased scroll-smooth`}
-			>
-				{children}
-			</body>
-		</html>
-	);
+  return (
+    <html lang="en">
+      <body
+        className={`${poppins.variable} ${montserrat.className} scroll-smooth antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
 }
